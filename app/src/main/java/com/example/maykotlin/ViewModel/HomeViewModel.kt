0c6ebf680e2d3a9d32aa855c356520b47f1b0d64
Deepak <deepakcse31@gmail.com>
2025-05-27 21:43:29 +0530

    coroutine

--- app/src/main/java/com/example/maykotlin/ViewModel/HomeViewModel.kt
+++ app/src/main/java/com/example/maykotlin/ViewModel/HomeViewModel.kt
@@ -1,7 +1,13 @@
 package com.example.maykotlin.ViewModel
 
+import android.util.Log
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelStore
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class HomeViewModel : ViewModel() {
     val _description = MutableLiveData<String>()
@@ -10,5 +16,18 @@ class HomeViewModel : ViewModel() {
     fun updateDescription(newDescription: String) {
         _description.value = newDescription
     }
+    init {
+        operation()
+    }
+    fun operation(){
+        GlobalScope.launch(Dispatchers.IO) {
+            doWork()
+        }
+    }
 
+    suspend fun doWork() {
+        // Simulate some work
+        Log.e("coroutine","doWork")
+        delay(2000)
+    }
 }
